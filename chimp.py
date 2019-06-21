2c9424846c3a71a83cffa94450752d615e169045
Jack Duval <jackcduval@gmail.com>
2019-06-21 19:32:20 -0400

    added load image func

---
+++ chimp.py
@@ -0,0 +1,19 @@
+import os, sys
+import pygame
+from pygame.locals import *
+
+if not pygame.font: print('Warning! Fonts disabled')
+if not pygame.mixer: print('Warning! Sound disabled')
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print("Cannot load image: ", name)
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
