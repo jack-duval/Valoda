f8fcd00d4957e5f06bd92e40386d67c301e0af7c
Jack Duval <jackcduval@gmail.com>
2019-06-24 09:11:05 -0400

    Add test tile_maps

--- chimp.py
+++ chimp.py
@@ -9,7 +9,7 @@ def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     try:
         image = pygame.image.load(fullname)
-    except pygame.error, message:
+    except pygame.error as message:
         print("Cannot load image: ", name)
     image = image.convert()
     if colorkey is not None:
@@ -17,3 +17,30 @@ def load_image(name, colorkey=None):
             colorkey = image.get_at((0,0))
         image.set_colorkey(colorkey, RLEACCEL)
     return image, image.get_rect()
+
+def load_sound(name):
+    class NoneSound:
+        def play(self): pass
+    if not pygame.mixer:
+        return NoneSound()
+    fullname = os.path.join('data', name)
+    try:
+        sound = pygame.mixer.Sound(fullname)
+    except pygame.error as message:
+        print('Cannot load sound: ', name)
+        raise SystemExit(message)
+    return sound
+
+class First(pygame.sprite.Sprite):
+    """Moves a clenched fist on the screen, follows mouse"""
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('fist.bmp', -1)
+        self.punching = 0
+
+    def update(self):
+        """Moves the fist based on mouse pos"""
+        pos = pygame.mouse.get_pos()
+        self.rect.midtop = pos
+        if self.punching:
+            self.rect.move_ip(5,10)
---
+++ tile_sets/musical.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="musical" tilewidth="32" tileheight="32" tilecount="1650" columns="50">
+ <image source="../../Seis meses/musical.jpg" width="1600" height="1067"/>
+</tileset>
